4ef1ff66611a73e46f68297c9a5e063086ff0506
sirdir <phddir@gmail.com>
2018-11-27 03:02:06 +0200

    some allure code, but didn"t create allure-report ffs

--- src/test/java/com.ingenico/RunCucumberTest.java
+++ src/test/java/com.ingenico/RunCucumberTest.java
@@ -3,6 +3,7 @@ package com.ingenico;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin ={"pretty" , "html:target/surefire-reports"})
+//@CucumberOptions(plugin ={"pretty", "html:target/surefire-reports"})
+@CucumberOptions(plugin ={"pretty"})
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
